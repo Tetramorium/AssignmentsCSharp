dd4245424da85d8c58dfb70d924f3b27d4394df7
Jelmer Mulder <9375891+Tetramorium@users.noreply.github.com>
2017-11-14 13:06:53 +0100

    RomanCalculator added

---
+++ Assignment.5.RomanCalculator/Assignment.5.RomanCalculator/App.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace Assignment._5.RomanCalculator
+{
+    partial class App
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(App));
+            this.bt_RomanI = new System.Windows.Forms.Button();
+            this.bt_RomanV = new System.Windows.Forms.Button();
+            this.bt_RomanL = new System.Windows.Forms.Button();
+            this.bt_RomanX = new System.Windows.Forms.Button();
+            this.bt_RomanC = new System.Windows.Forms.Button();
+            this.bt_RomanD = new System.Windows.Forms.Button();
+            this.bt_RomanM = new System.Windows.Forms.Button();
+            this.tb_Input = new System.Windows.Forms.TextBox();
+            this.bt_Add = new System.Windows.Forms.Button();
+            this.bt_Substract = new System.Windows.Forms.Button();
+            this.bt_Multiply = new System.Windows.Forms.Button();
+            this.bt_Divide = new System.Windows.Forms.Button();
+            this.bt_Result = new System.Windows.Forms.Button();
+            this.bt_Clear = new System.Windows.Forms.Button();
+            this.tTip_RomanNumerals = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // bt_RomanI
+            // 
+            this.bt_RomanI.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_RomanI.Location = new System.Drawing.Point(12, 77);
+            this.bt_RomanI.Name = "bt_RomanI";
+            this.bt_RomanI.Size = new System.Drawing.Size(50, 50);
+            this.bt_RomanI.TabIndex = 0;
+            this.bt_RomanI.Text = "I";
+            this.bt_RomanI.UseVisualStyleBackColor = true;
+            this.bt_RomanI.Click += new System.EventHandler(this.bt_RomanI_Click);
+            // 
+            // bt_RomanV
+            // 
+            this.bt_RomanV.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_RomanV.Location = new System.Drawing.Point(68, 77);
+            this.bt_RomanV.Name = "bt_RomanV";
+            this.bt_RomanV.Size = new System.Drawing.Size(50, 50);
+            this.bt_RomanV.TabIndex = 1;
+            this.bt_RomanV.Text = "V";
+            this.bt_RomanV.UseVisualStyleBackColor = true;
+            this.bt_RomanV.Click += new System.EventHandler(this.bt_RomanV_Click);
+            // 
+            // bt_RomanL
+            // 
+            this.bt_RomanL.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_RomanL.Location = new System.Drawing.Point(12, 133);
+            this.bt_RomanL.Name = "bt_RomanL";
+            this.bt_RomanL.Size = new System.Drawing.Size(50, 50);
+            this.bt_RomanL.TabIndex = 2;
+            this.bt_RomanL.Text = "L";
+            this.bt_RomanL.UseVisualStyleBackColor = true;
+            this.bt_RomanL.Click += new System.EventHandler(this.bt_RomanL_Click);
+            // 
+            // bt_RomanX
+            // 
+            this.bt_RomanX.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_RomanX.Location = new System.Drawing.Point(124, 77);
+            this.bt_RomanX.Name = "bt_RomanX";
+            this.bt_RomanX.Size = new System.Drawing.Size(50, 50);
+            this.bt_RomanX.TabIndex = 3;
+            this.bt_RomanX.Text = "X";
+            this.bt_RomanX.UseVisualStyleBackColor = true;
+            this.bt_RomanX.Click += new System.EventHandler(this.bt_RomanX_Click);
+            // 
+            // bt_RomanC
+            // 
+            this.bt_RomanC.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_RomanC.Location = new System.Drawing.Point(68, 133);
+            this.bt_RomanC.Name = "bt_RomanC";
+            this.bt_RomanC.Size = new System.Drawing.Size(50, 50);
+            this.bt_RomanC.TabIndex = 4;
+            this.bt_RomanC.Text = "C";
+            this.bt_RomanC.UseVisualStyleBackColor = true;
+            this.bt_RomanC.Click += new System.EventHandler(this.bt_RomanC_Click);
+            // 
+            // bt_RomanD
+            // 
+            this.bt_RomanD.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_RomanD.Location = new System.Drawing.Point(124, 133);
+            this.bt_RomanD.Name = "bt_RomanD";
+            this.bt_RomanD.Size = new System.Drawing.Size(50, 50);
+            this.bt_RomanD.TabIndex = 5;
+            this.bt_RomanD.Text = "D";
+            this.bt_RomanD.UseVisualStyleBackColor = true;
+            this.bt_RomanD.Click += new System.EventHandler(this.bt_RomanD_Click);
+            // 
+            // bt_RomanM
+            // 
+            this.bt_RomanM.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_RomanM.Location = new System.Drawing.Point(12, 189);
+            this.bt_RomanM.Name = "bt_RomanM";
+            this.bt_RomanM.Size = new System.Drawing.Size(50, 50);
+            this.bt_RomanM.TabIndex = 6;
+            this.bt_RomanM.Text = "M";
+            this.bt_RomanM.UseVisualStyleBackColor = true;
+            this.bt_RomanM.Click += new System.EventHandler(this.bt_RomanM_Click);
+            // 
+            // tb_Input
+            // 
+            this.tb_Input.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_Input.Location = new System.Drawing.Point(12, 12);
+            this.tb_Input.Name = "tb_Input";
+            this.tb_Input.Size = new System.Drawing.Size(291, 32);
+            this.tb_Input.TabIndex = 7;
+            // 
+            // bt_Add
+            // 
+            this.bt_Add.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_Add.Location = new System.Drawing.Point(197, 133);
+            this.bt_Add.Name = "bt_Add";
+            this.bt_Add.Size = new System.Drawing.Size(50, 50);
+            this.bt_Add.TabIndex = 8;
+            this.bt_Add.Text = "+";
+            this.bt_Add.UseVisualStyleBackColor = true;
+            this.bt_Add.Click += new System.EventHandler(this.bt_Add_Click);
+            // 
+            // bt_Substract
+            // 
+            this.bt_Substract.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_Substract.Location = new System.Drawing.Point(253, 133);
+            this.bt_Substract.Name = "bt_Substract";
+            this.bt_Substract.Size = new System.Drawing.Size(50, 50);
+            this.bt_Substract.TabIndex = 9;
+            this.bt_Substract.Text = "-";
+            this.bt_Substract.UseVisualStyleBackColor = true;
+            this.bt_Substract.Click += new System.EventHandler(this.bt_Substract_Click);
+            // 
+            // bt_Multiply
+            // 
+            this.bt_Multiply.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_Multiply.Location = new System.Drawing.Point(197, 189);
+            this.bt_Multiply.Name = "bt_Multiply";
+            this.bt_Multiply.Size = new System.Drawing.Size(50, 50);
+            this.bt_Multiply.TabIndex = 10;
+            this.bt_Multiply.Text = "*";
+            this.bt_Multiply.UseVisualStyleBackColor = true;
+            this.bt_Multiply.Click += new System.EventHandler(this.bt_Multiply_Click);
+            // 
+            // bt_Divide
+            // 
+            this.bt_Divide.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_Divide.Location = new System.Drawing.Point(253, 189);
+            this.bt_Divide.Name = "bt_Divide";
+            this.bt_Divide.Size = new System.Drawing.Size(50, 50);
+            this.bt_Divide.TabIndex = 11;
+            this.bt_Divide.Text = "/";
+            this.bt_Divide.UseVisualStyleBackColor = true;
+            this.bt_Divide.Click += new System.EventHandler(this.bt_Divide_Click);
+            // 
+            // bt_Result
+            // 
+            this.bt_Result.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_Result.Location = new System.Drawing.Point(253, 77);
+            this.bt_Result.Name = "bt_Result";
+            this.bt_Result.Size = new System.Drawing.Size(50, 50);
+            this.bt_Result.TabIndex = 12;
+            this.bt_Result.Text = "=";
+            this.bt_Result.UseVisualStyleBackColor = true;
+            this.bt_Result.Click += new System.EventHandler(this.bt_Result_Click);
+            // 
+            // bt_Clear
+            // 
+            this.bt_Clear.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_Clear.Location = new System.Drawing.Point(197, 77);
+            this.bt_Clear.Name = "bt_Clear";
+            this.bt_Clear.Size = new System.Drawing.Size(50, 50);
+            this.bt_Clear.TabIndex = 13;
+            this.bt_Clear.Text = "Ce";
+            this.bt_Clear.UseVisualStyleBackColor = true;
+            this.bt_Clear.Click += new System.EventHandler(this.bt_Clear_Click);
+            // 
+            // tTip_RomanNumerals
+            // 
+            this.tTip_RomanNumerals.AutomaticDelay = 100;
+            this.tTip_RomanNumerals.AutoPopDelay = 1000;
+            this.tTip_RomanNumerals.InitialDelay = 50;
+            this.tTip_RomanNumerals.ReshowDelay = 20;
+            // 
+            // App
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 249);
+            this.Controls.Add(this.bt_Clear);
+            this.Controls.Add(this.bt_Result);
+            this.Controls.Add(this.bt_Divide);
+            this.Controls.Add(this.bt_Multiply);
+            this.Controls.Add(this.bt_Substract);
+            this.Controls.Add(this.bt_Add);
+            this.Controls.Add(this.tb_Input);
+            this.Controls.Add(this.bt_RomanM);
+            this.Controls.Add(this.bt_RomanD);
+            this.Controls.Add(this.bt_RomanC);
+            this.Controls.Add(this.bt_RomanX);
+            this.Controls.Add(this.bt_RomanL);
+            this.Controls.Add(this.bt_RomanV);
+            this.Controls.Add(this.bt_RomanI);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "App";
+            this.Text = "Roman Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_RomanI;
+        private System.Windows.Forms.Button bt_RomanV;
+        private System.Windows.Forms.Button bt_RomanL;
+        private System.Windows.Forms.Button bt_RomanX;
+        private System.Windows.Forms.Button bt_RomanC;
+        private System.Windows.Forms.Button bt_RomanD;
+        private System.Windows.Forms.Button bt_RomanM;
+        private System.Windows.Forms.TextBox tb_Input;
+        private System.Windows.Forms.Button bt_Add;
+        private System.Windows.Forms.Button bt_Substract;
+        private System.Windows.Forms.Button bt_Multiply;
+        private System.Windows.Forms.Button bt_Divide;
+        private System.Windows.Forms.Button bt_Result;
+        private System.Windows.Forms.Button bt_Clear;
+        private System.Windows.Forms.ToolTip tTip_RomanNumerals;
+    }
+}
+
---
+++ Assignment.5.RomanCalculator/Assignment.5.RomanCalculator/App.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment._5.RomanCalculator
+{
+    public partial class App : Form
+    {
+        public enum Operations
+        {
+            Add,
+            Substract,
+            Multiply,
+            Divide,
+            Nothing
+        }
+
+        private Dictionary<string, int> RomanNumbers;
+
+        private bool operatorIsClicked;
+        private Operations currentOperation;
+        private int currentValue;
+        private int oldValue;
+
+        public App()
+        {
+            InitializeComponent();
+
+            this.currentOperation = Operations.Nothing;
+
+            this.RomanNumbers = new Dictionary<string, int>();
+
+            this.tTip_RomanNumerals.SetToolTip(this.bt_RomanI, "1");
+            this.tTip_RomanNumerals.SetToolTip(this.bt_RomanV, "5");
+            this.tTip_RomanNumerals.SetToolTip(this.bt_RomanX, "10");
+            this.tTip_RomanNumerals.SetToolTip(this.bt_RomanL, "50");
+            this.tTip_RomanNumerals.SetToolTip(this.bt_RomanC, "100");
+            this.tTip_RomanNumerals.SetToolTip(this.bt_RomanD, "500");
+            this.tTip_RomanNumerals.SetToolTip(this.bt_RomanM, "1000");
+
+            RomanNumbers.Add("M", 1000);
+            RomanNumbers.Add("CM", 900);
+            RomanNumbers.Add("D", 500);
+            RomanNumbers.Add("CD", 400);
+            RomanNumbers.Add("C", 100);
+            RomanNumbers.Add("XC", 90);
+            RomanNumbers.Add("L", 50);
+            RomanNumbers.Add("XL", 40);
+            RomanNumbers.Add("X", 10);
+            RomanNumbers.Add("IX", 9);
+            RomanNumbers.Add("V", 5);
+            RomanNumbers.Add("IV", 4);
+            RomanNumbers.Add("I", 1);
+        }
+
+        private void AddToInput(string Input)
+        {
+            if (operatorIsClicked)
+            {
+                oldValue = currentValue;
+                currentValue = 0;
+
+                this.currentValue += RomanNumbers[Input];
+                this.tb_Input.Text = ConvertIntToRoman(currentValue);
+
+                operatorIsClicked = false;
+            }
+            else
+            {
+                this.currentValue += RomanNumbers[Input];
+                this.tb_Input.Text = ConvertIntToRoman(currentValue);
+            }
+        }
+
+        private string ConvertIntToRoman(int number)
+        {
+            StringBuilder str = new StringBuilder();
+
+            foreach (KeyValuePair<string, int> i in this.RomanNumbers)
+            {
+                int temp = number / i.Value;
+                for (int j = 0; j < temp; j++)
+                {
+                    str.Append(i.Key);
+                }
+
+                number -= temp * i.Value;
+            }
+
+            return str.ToString();
+        }
+
+        private void OperatorClicked(Operations operation)
+        {
+            operatorIsClicked = true;
+
+            if (operation != Operations.Nothing)
+            {
+                if (currentOperation != Operations.Nothing)
+                    Calculate();
+            }
+
+            currentOperation = operation;
+        }
+
+        private void Calculate()
+        {
+            switch (currentOperation)
+            {
+                case Operations.Add:
+                    currentValue += oldValue;
+                    break;
+                case Operations.Substract:
+                    currentValue -= oldValue;
+                    break;
+                case Operations.Multiply:
+                    currentValue *= oldValue;
+                    break;
+                case Operations.Divide:
+                    try
+                    {
+                        currentValue = oldValue / currentValue;
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        MessageBox.Show("Don't divide by zero!");
+
+                        Clear();
+                    }                   
+                    break;
+            }
+            operatorIsClicked = true;
+            currentOperation = Operations.Nothing;
+            this.tb_Input.Text = ConvertIntToRoman(currentValue);
+        }
+
+        private void bt_Add_Click(object sender, EventArgs e)
+        {
+            OperatorClicked(Operations.Add);
+        }
+
+        private void bt_Substract_Click(object sender, EventArgs e)
+        {
+            OperatorClicked(Operations.Substract);
+        }
+
+        private void bt_Multiply_Click(object sender, EventArgs e)
+        {
+            OperatorClicked(Operations.Multiply);
+        }
+
+        private void bt_Divide_Click(object sender, EventArgs e)
+        {
+            OperatorClicked(Operations.Divide);
+        }
+
+        private void bt_RomanI_Click(object sender, EventArgs e)
+        {
+            AddToInput("I");
+        }
+
+        private void bt_RomanV_Click(object sender, EventArgs e)
+        {
+            AddToInput("V");
+        }
+
+        private void bt_RomanL_Click(object sender, EventArgs e)
+        {
+            AddToInput("L");
+        }
+
+        private void bt_RomanX_Click(object sender, EventArgs e)
+        {
+            AddToInput("X");
+        }
+
+        private void bt_RomanD_Click(object sender, EventArgs e)
+        {
+            AddToInput("D");
+        }
+
+        private void bt_RomanM_Click(object sender, EventArgs e)
+        {
+            AddToInput("M");
+        }
+
+        private void bt_RomanC_Click(object sender, EventArgs e)
+        {
+            AddToInput("C");
+        }
+
+        private void bt_Clear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void Clear()
+        {
+            this.tb_Input.Text = "";
+            this.currentValue = 0;
+            this.operatorIsClicked = false;
+            this.currentOperation = Operations.Nothing;
+        }
+
+        private void bt_Result_Click(object sender, EventArgs e)
+        {
+            Calculate();
+        }
+    }
+}
